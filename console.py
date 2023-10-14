25c80c67e79004482a763bf68b34fd0881e1e2ac
KENC0DE <kenansameseret@gmail.com>
2023-10-14 20:02:41 +0530

    console

--- console.py
+++ console.py
@@ -51,6 +51,34 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print(storage.all()[key])
 
+    def do_destroy(self, arg):
+        """ Deletes an instance based on the class name and id"""
+        if arg == "" or arg is None:
+            print("** class name missing **")
+        else:
+            arg = arg.split(' ')
+            if storage.load_class(arg[0]) is None:
+                print("** class doesn't exist **")
+            elif len(arg) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(arg[0], arg[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+                    storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        if arg and storage.load_class(arg) is None:
+            print("** class doesn't exist **")
+        else:
+            objs = storage.all()
+            lst = []
+            for p in objs:
+                lst.append(str(objs[p]))
+            print(lst)
 
 
 if __name__ == '__main__':
